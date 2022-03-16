8e886e5529d00f68c40f7acf41edfe1a5cd8dfb6
renan04lima <renan04lima@outlook.com>
2022-03-16 16:39:26 -0300

    chore: config jest

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,62 @@
-module.exports = {
-  roots: ['<rootDir>/server/tests'],
-  collectCoverageFrom: [
-    '<rootDir>/server/**/*.ts'
-  ],
+const defaultConfig = {
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
-  testEnvironment: 'node',
+  coverageReporters: [
+    'text',
+    'lcov'
+  ],
+  coverageThreshold: {
+    global: {
+      branch: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  maxWorkers: '50%',
+  watchPathIgnorePatterns: [
+    'node_modules'
+  ],
+  transformIgnorePatterns: [
+    'node_modules'
+  ],
   transform: {
     '.+\\.ts$': 'ts-jest'
-  },
-  moduleNameMapper: {
-    '@/(.*)': '<rootDir>/server/$1'
   }
+}
+
+module.exports = {
+  projects: [
+    {
+      ...defaultConfig,
+      testEnvironment: 'node',
+      displayName: 'backend',
+      collectCoverageFrom: [
+        'server/',
+        '!server/index.ts'
+      ],
+      transformIgnorePatterns: [
+        ...defaultConfig.transformIgnorePatterns,
+        'public'
+      ],
+      testMatch: [
+        '**/tests/**/server/**/*.test.ts'
+      ]
+    },
+    {
+      ...defaultConfig,
+      testEnvironment: 'jsdom',
+      displayName: 'frontend',
+      collectCoverageFrom: [
+        'public/'
+      ],
+      transformIgnorePatterns: [
+        ...defaultConfig.transformIgnorePatterns,
+        'server'
+      ],
+      testMatch: [
+        '**/tests/**/public/**/*.test.ts'
+      ]
+    }
+  ]
 }
