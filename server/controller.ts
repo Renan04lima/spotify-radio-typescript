18a05a39168f63feb373c73195792b4b1731040f
renan04lima <renan04lima@outlook.com>
2022-03-17 18:58:17 -0300

    feat: add createClientStream in controller layer

--- server/controller.ts
+++ server/controller.ts
@@ -1,5 +1,7 @@
 import { ReadStream } from 'fs'
+import { logger } from './util'
 import { Service } from './service'
+import { PassThrough } from 'stream'
 
 export class Controller {
   private readonly service: Service
@@ -13,5 +15,22 @@ export class Controller {
     stream: ReadStream
   }> {
     return this.service.getFileStream(filename)
+  }
+
+  createClientStream (): {stream: PassThrough, onClose: () => void} {
+    const {
+      id,
+      clientStream
+    } = this.service.createClientStream()
+
+    const onClose = (): void => {
+      logger.info(`closing connection of ${id}`)
+      this.service.removeClientStream(id)
+    }
+
+    return {
+      stream: clientStream,
+      onClose
+    }
   }
 }
