3ddbd07f8e978106817cedd28b8d59d93cfc4f6d
renan04lima <renan04lima@outlook.com>
2022-03-19 15:30:05 -0300

    test: ensure GET /stream receive data stream if the process is playing

--- tests/e2e/server/api.test.ts
+++ tests/e2e/server/api.test.ts
@@ -9,6 +9,14 @@ const getAvailablePort = portfinder.getPortPromise
 const RETENTION_DATA_PERIOD = 200
 
 describe('API E2E Suite Test', () => {
+  const commandResponse = JSON.stringify({
+    result: 'ok'
+  })
+  const possibleCommands = {
+    start: 'start',
+    stop: 'stop'
+  }
+
   function pipeAndReadStreamData (stream: Test, onChunk: (x: any) => void): Writable {
     const transform = new Transform({
       transform (chunk, enc, cb) {
@@ -40,6 +48,18 @@ describe('API E2E Suite Test', () => {
       })
     }
 
+    function commandSender (testServer: SuperTest<Test>): { send: (command: string) => Promise<void> } {
+      return {
+        async send (command: string) {
+          const response = await testServer.post('/controller')
+            .send({
+              command
+            })
+
+          expect(response.text).toStrictEqual(commandResponse)
+        }
+      }
+    }
     test('it should not receive data stream if the process is not playing', async () => {
       const server = await getTestServer()
       const onChunk = jest.fn()
@@ -51,6 +71,29 @@ describe('API E2E Suite Test', () => {
       await setTimeout(RETENTION_DATA_PERIOD)
       server.kill()
       expect(onChunk).not.toHaveBeenCalled()
+    })
+    test('it should receive data stream if the process is playing', async () => {
+      const server = await getTestServer()
+      const onChunk = jest.fn()
+      const {
+        send
+      } = commandSender(server.testServer)
+      pipeAndReadStreamData(
+        server.testServer.get('/stream'),
+        onChunk
+      )
+
+      await send(possibleCommands.start)
+      // eslint-disable-next-line @typescript-eslint/no-implied-eval
+      await setTimeout(RETENTION_DATA_PERIOD)
+      await send(possibleCommands.stop)
+      const [
+        [buffer]
+      ] = onChunk.mock.calls
+      expect(buffer).toBeInstanceOf(Buffer)
+      expect(buffer.length).toBeGreaterThan(1000)
+
+      server.kill()
     })
   })
 })
