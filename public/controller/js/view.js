fcc01b0575d7977b153df740dfcca399582236ab
renan04lima <renan04lima@outlook.com>
2022-03-20 18:53:05 -0300

    feat: adding function to other controller buttons

--- public/controller/js/view.js
+++ public/controller/js/view.js
@@ -2,7 +2,9 @@ export default class View {
   constructor() {
     this.btnStart = document.getElementById('start')
     this.btnStop = document.getElementById('stop')
+    this.buttons = () => Array.from(document.querySelectorAll('button'))
 
+    this.ignoreButtons = new Set(['unassigned'])
     async function onBtnClick() { }
     this.onBtnClick = onBtnClick
   }
@@ -33,5 +35,50 @@ export default class View {
   }) {
     const btnText = innerText
     await this.onBtnClick(btnText)
+    this.toggleBtnStart()
+    this.changeCommandBtnsVisibility(false)
+
+    this.buttons()
+      .filter(btn => this.notIsUnassignedButton(btn))
+      .forEach(this.setupBtnAction.bind(this))
+  }
+
+  setupBtnAction(btn) {
+    const text = btn.innerText.toLowerCase()
+    if (text.includes('start')) return;
+
+    if (text.includes('stop')) {
+      btn.onclick = this.onStopBtn.bind(this)
+      return
+    }
+
+    btn.onclick = this.onCommandClick.bind(this)
+  }
+
+  onStopBtn({
+    srcElement: {
+      innerText
+    }
+  }) {
+    this.toggleBtnStart(false)
+    this.changeCommandBtnsVisibility(true)
+
+    return this.onBtnClick(innerText)
+  }
+
+  notIsUnassignedButton(btn) {
+    const classes = Array.from(btn.classList)
+    return !(!!classes.find(item => this.ignoreButtons.has(item)))
+  }
+
+  toggleBtnStart(active = true) {
+    if (active) {
+      this.btnStart.classList.add('hidden')
+      this.btnStop.classList.remove('hidden')
+      return;
+    }
+
+    this.btnStop.classList.add('hidden')
+    this.btnStart.classList.remove('hidden')
   }
 }
